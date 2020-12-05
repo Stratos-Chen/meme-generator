35fa9f73b62a363e109b34bc8aecdec5f90c1204
Stratos-Chen <twinschen55@gmail.com>
2020-12-04 23:09:34 -0800

    list of created memes now viewable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import MemeGenerator from "./MemeGenerator";
+import MemeList from "./MemeList";
 
 function App() {
   return (
     <div>
       <Header />
       <MemeGenerator />
+      <MemeList />
     </div>
   );
 }
