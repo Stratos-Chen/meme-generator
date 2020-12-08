ee433cd4dab2c4c3444c3a9e7dc32f2b346b10cc
Stratos-Chen <twinschen55@gmail.com>
2020-12-07 18:38:34 -0800

    made the app a multi-page app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Header from "./Header";
 import MemeGenerator from "./MemeGenerator";
 import EditMeme from "./EditMeme";
 import MemeList from "./MemeList";
+import Navigation from "./Navigation";
+import TestPage from "./components/TestPage";
+// import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <MemeGenerator />
-      <EditMeme />
-      <MemeList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Navigation />
+          <Switch>
+            <Route path="/memegenerator" component={MemeGenerator} />
+            <Route path="/editmeme" component={EditMeme} />
+            <Route path="/memelist" component={MemeList} />
+            <Route path="/testpage" component={TestPage} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 export default App;
---
+++ src/components/TestPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const testpage = () => {
+  return (
+    <div>
+      <h1>Testing Page</h1>
+      <p>Testing, testing</p>
+    </div>
+  );
+};
+
+export default testpage;
